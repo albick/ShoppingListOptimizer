76504ec1d07e08b8ff0e6009059c019a375e7eeb
albick99 <khhun12@gmail.com>
2023-11-01 16:00:50 +0100

    separate req res

---
+++ ShoppingListOptimizerAPI/Requests/RefreshTokenRequest.cs
@@ -0,0 +1,9 @@
+﻿using System.Text.Json.Serialization;
+
+namespace ShoppingListOptimizerAPI.Requests
+{
+    public class RefreshTokenRequest
+    {
+        [JsonPropertyName("refreshToken")] public string RefreshToken { get; set; }
+    }
+}
---
+++ ShoppingListOptimizerAPI/Responses/LoginResponse.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json.Serialization;
+
+namespace ShoppingListOptimizerAPI.Responses
+{
+    public class LoginResponse
+    {
+        [JsonPropertyName("username")] public string UserName { get; set; }
+
+        [JsonPropertyName("role")] public string Role { get; set; }
+
+        [JsonPropertyName("originalUserName")] public string OriginalUserName { get; set; }
+
+        [JsonPropertyName("accessToken")] public string AccessToken { get; set; }
+
+        [JsonPropertyName("refreshToken")] public string RefreshToken { get; set; }
+
+        [JsonPropertyName("email")] public string Email { get; set; }
+    }
+}
