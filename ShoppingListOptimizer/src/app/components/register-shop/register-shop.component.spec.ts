4c408996d219a82c09bea9b9aa3f2ff9bf914530
albick99 <khhun12@gmail.com>
2023-11-02 19:47:27 +0100

    registrations done

---
+++ ShoppingListOptimizer/src/app/components/register-shop/register-shop.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { RegisterShopComponent } from './register-shop.component';
+
+describe('RegisterShopComponent', () => {
+  let component: RegisterShopComponent;
+  let fixture: ComponentFixture<RegisterShopComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ RegisterShopComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(RegisterShopComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ ShoppingListOptimizer/src/app/components/register-shop/register-shop.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-register-shop',
+  templateUrl: './register-shop.component.html',
+  styleUrls: ['./register-shop.component.css']
+})
+export class RegisterShopComponent {
+
+}
