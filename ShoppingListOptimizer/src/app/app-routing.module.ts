af15802aca74fca07443e19b0ae41776d3de2834
albick99 <khhun12@gmail.com>
2023-11-02 22:14:44 +0100

    barcode scanner component

--- ShoppingListOptimizer/src/app/app-routing.module.ts
+++ ShoppingListOptimizer/src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import {LoginComponent} from "./components/login/login.component";
 import {RegisterComponent} from "./components/register/register.component";
 import { RegisterShopComponent } from './components/register-shop/register-shop.component';
 import { HomeComponent } from './components/home/home.component';
+import { BarcodeScannerComponent } from './components/barcode-scanner/barcode-scanner.component';
 
 const routes: Routes = [
   {path: 'login', component: LoginComponent},
   {path: 'register', component: RegisterComponent},
   {path: 'register-shop', component: RegisterShopComponent},
   {path: 'home', component: HomeComponent},
+  {path: 'scan-barcode', component: BarcodeScannerComponent},
   {path: '', redirectTo: 'login', pathMatch: 'full'}
 ];
 
